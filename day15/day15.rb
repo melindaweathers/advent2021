d3c938836f7dc463d6b72784e47fba3b2d6a4d65
Melinda Weathers <melindaweathers@gmail.com>
2021-12-24 14:51:32 -0500

    Day 15 (slow :see_no_evil:)

--- day15/day15.rb
+++ day15/day15.rb
@@ -2,17 +2,42 @@ ADJS = [[-1, 0], [1, 0], [0, -1], [0, 1]]
 Point = Struct.new(:risk, :distance, :x, :y)
 
 class Grid
-  def initialize(filename)
+  def initialize(filename, full_map = false)
     @unvisited = []
     @grid = IO.readlines(filename).map.with_index do |line, y|
       line.chomp.split('').map.with_index do |val, x|
-        point = Point.new(val.to_i, 100000000, x, y)
-        @unvisited << point
+        point = Point.new(val.to_i, 10000000, x, y)
+        @unvisited << point unless full_map
         point
       end
     end
+    if full_map
+      grid_height = @grid.length
+      grid_width = @grid.first.length
+      giant_grid = Array.new(grid_height * 5)
+      0.upto(4) do |row_dup|
+        0.upto(4) do |col_dup|
+          0.upto(grid_height - 1) do |row|
+            0.upto(grid_width - 1) do |col|
+              x = col + col_dup * grid_width
+              y = row + row_dup * grid_width
+              risk = @grid[row][col].risk + col_dup + row_dup
+              point = Point.new(((risk - 1) % 9) + 1, 10000000000, x, y)
+              giant_grid[y] ||= Array.new(grid_width * 5)
+              giant_grid[y][x] = point
+              @unvisited << point
+            end
+          end
+        end
+      end
+      @grid = giant_grid
+    end
     @grid[0][0].distance = 0
-    @unvisited.sort_by{|point| point.distance}
+    fix_sort
+  end
+
+  def fix_sort
+    @unvisited.sort_by!{|point| -1*point.distance}
   end
 
   def neighbors(curpoint)
@@ -30,21 +55,27 @@ class Grid
     @width ||= @grid.first.length
   end
 
-  def print_distances
+  def print_risks
     @grid.each do |row|
-      puts row.map(&:distance).join(' ')
+      puts row.map(&:risk).join(' ')
     end
   end
 
   def lowest_risk_path
     loop do
       break if @unvisited.empty?
-      u = @unvisited.min_by{|point| point.distance}
-      @unvisited.delete(u)
+      u = @unvisited.pop
       neighbors(u).each do |neighbor|
         alt = u.distance + neighbor.risk
         if alt < neighbor.distance
           neighbor.distance = alt
+          # Move neighbor to new position
+          i = @unvisited.index(neighbor)
+          loop do
+            break if i >= @unvisited.length - 1 || @unvisited[i + 1].distance < neighbor.distance
+            @unvisited[i], @unvisited[i + 1] = @unvisited[i + 1], @unvisited[i]
+            i += 1
+          end
         end
       end
     end
@@ -53,4 +84,9 @@ class Grid
 end
 
 puts Grid.new('input-test.txt').lowest_risk_path
-puts Grid.new('input.txt').lowest_risk_path
+#puts Grid.new('input.txt').lowest_risk_path
+
+#Grid.new('input-test.txt', true).print_risks
+puts Grid.new('input-test.txt', true).lowest_risk_path
+puts Grid.new('input.txt', true).lowest_risk_path
+
